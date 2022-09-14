329786c0a491d052af635a248f01fdad21a939f0
Brandon Chairez <83408164+bchairz@users.noreply.github.com>
2022-09-13 21:48:11 -0400

    Development (#6)
    * modified README.md
    
    * merge main into development branch (#3)
    
    * modified README.md (#1)
    
    * modified README.md (#2)
    
    * - updated get_number_of_teams function
    
    * added functionality for getting team names
    
    implemented get_team_names function
    
    * implemented get_number_of_games_played
    
    * Typo
    
    * added more functionality to this project
    
    added docstrings

--- tournament_game_generator.py
+++ tournament_game_generator.py
@@ -1,6 +1,8 @@
-# Write your code here.
-
-def get_number_of_teams():
+def get_number_of_teams() -> int:
+    '''
+    function that gets the number of teams to play in the tournament
+    number of teams must be at least 2 and an even amount
+    '''
     MIN_NUM_TEAMS = 2
     while True:
         num_teams = int(input('Enter the number of teams in the tournament: '))
@@ -11,20 +13,64 @@ def get_number_of_teams():
     
     return num_teams
 
-def get_team_names(num_teams):
-    pass
+def get_team_names(num_teams) -> dict:
+    '''
+    function that gets the name of the team for each team
+    '''
+    team_names = {}
+    for team in range(1,num_teams+1):
+        team_num = f'team #{str(team)}'
 
+        while True:
+            name_of_team = input(f'Enter a name for {team_num}: ')
+            if len(name_of_team) < 2:
+                print("Team names must have at least 2 characters, try again.")
+            elif len(name_of_team.split()) > 2:
+                print("Team names may have at most 2 words, try again.")
+            else: break
+        team_names[team] = name_of_team
+    
+    return team_names
 
-def get_number_of_games_played(num_teams):
-    pass
+def get_number_of_games_played(num_teams) -> int:
+    '''
+    function that gets the number of games played by each team
+    each team must play at least once
+    '''
+    while True:
+        games_played = int(input("Enter the number of games played by each team: "))
 
+        if games_played >= num_teams - 1:
+            break
+        
+        print("Invalid number of games. Each team plays each other at least" + \
+              " once in the regular season")
+
+    return games_played
 
 def get_team_wins(team_names, games_played):
-    pass
+    '''
+    function that gets the numbe of wins for each team
+    constraints: wins must be between 0 and number of games played 
+    '''
+    team_wins = []
 
+    for team in team_names.values():
+        while True:
+            wins = int(input(f"Enter the number of wins Team {team} had: "))
 
-# It is not necessary to use the functions defined above. There are simply here
-# to help give your code some structure and provide a starting point.
+            if wins >= 0 and wins <= games_played:
+                break
+            elif wins < 0:
+                print("The minimum number of wins is 0, try again.")
+            elif wins > games_played:
+                print(f"The maximum number of wins is {games_played}, try again.")
+        
+        team_wins.append((team, wins))
+        print(team_wins)
+    
+    return team_wins
+
 num_teams = get_number_of_teams()
 team_names = get_team_names(num_teams)
 games_played = get_number_of_games_played(num_teams)
